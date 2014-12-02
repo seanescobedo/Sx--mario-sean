0cd4f703b12868f7d6c935ec51fc5ab94f308090
sean escobedo <seancandoit68@gmail.com>
2014-12-02 09:07:26 -0800

    more work bruh < .><.>                                       0 {{{}}}}}

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -132,9 +132,48 @@ game.BadGuy = me.Entity.extend({
         //^these numbers here can change your hitbox widtth & height^\\
             }
         }]);  
+    
+    this.spritewidth = 60;
+    var width = settings.width;
+    x = this.pos.x;
+    this.startX = x;
+    this.endX = x + width - this.spritewidth;
+    this.pos.x = x + width - this.spritewidth;
+    this.updateBounds();
+    
+    this.alwaysUpdate = true;
+    
+    this.walkLeft = false;
+    this.alive = true;
+    this.type = "badguy";
+    
+    //this.renderable.addAnimation("run", [0, 1, 2], 80);
+    //this.renderable.setCurrentAnimation("run");
+    
+    this.body.setVelocity(4, 6);
     },
     
     update: function(delta){
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+        
+        if(this.alive){
+            if(this.walkLeft && this.pos.x <= this.startX){
+                this.walkLeft = false;
+            }else if(!this.walkLeft && this.pos.x >= this.endX){
+                this.walkLeft = true;
+            }
+            this.flipX(!this.walkLeft);
+            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body. accel.x * me.timer.tick;
+        }else{
+            me.game.world.removeChild(this);
+        }
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;  
+    },
+    
+    collideHandler: function(){
         
     }
     
